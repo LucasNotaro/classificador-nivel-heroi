f380962d2deb4939e171d4cb150be2878663e4c7
LucasNotaro <lucas.sn@live.com>
2024-11-02 03:07:28 -0300

    Uso de um objeto para representar o herói com nome e XP

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-let nome = "Notaro";
-let xp = 7500;
+let heroi = { nome: "Notaro", xp: 7500 };
 
-if (xp < 1000) {
+if (heroi.xp < 1000) {
     nivel = "Ferro";
-} else if (xp <= 2000) {
+} else if (heroi.xp <= 2000) {
     nivel = "Bronze";
-} else if (xp <= 5000) {
+} else if (heroi.xp <= 5000) {
     nivel = "Prata";
-} else if (xp <= 7000) {
+} else if (heroi.xp <= 7000) {
     nivel = "Ouro";
-} else if (xp <= 8000) {
+} else if (heroi.xp <= 8000) {
     nivel = "Platina";
-} else if (xp <= 9000) {
+} else if (heroi.xp <= 9000) {
     nivel = "Ascendente";
-} else if (xp <= 10000) {
+} else if (heroi.xp <= 10000) {
     nivel = "Imortal";
 } else {
     nivel = "Radiante";
 }
 
-console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+console.log(`O Herói de nome ${heroi.nome} está no nível de ${nivel}`);
