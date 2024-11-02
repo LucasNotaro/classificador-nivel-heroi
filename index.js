a7e9596547bd56381d01da500c8837a6f0a0cde5
LucasNotaro <lucas.sn@live.com>
2024-11-02 02:35:06 -0300

    Estrutura if else completa para classificar o nível

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ if (Notaro < 1000) {
     "Ferro";
 } else if (Notaro <= 2000) {
     "Bronze";
+} else if (Notaro <= 5000) {
+    "Prata";
+} else if (Notaro <= 7000) {
+    "Ouro";
+} else if (Notaro <= 8000) {
+    "Platina";
+} else if (Notaro <= 9000) {
+   "Ascendente";
+} else if (Notaro <= 10000) {
+    "Imortal";
 } else {
     "Radiante";
 }
